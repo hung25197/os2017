561907d5a8a8a63ffabf8bbe5998a326da56768e
hung25197 <hungnga25197@gmail.com>
2017-02-10 03:13:28 +0700

    Report 04 practical work

---
+++ 04.practical.work.fork.exec.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() 
+{
+	printf("Main parrent process\n");
+	int pid = fork();
+	if (pid == 0) 
+		{
+		printf("I am child after fork(), launching ps -ef\n");
+		char *args[]= { "/bin/ps", "-ef" , NULL};
+		execvp("/bin/ps", args);
+		}
+	else printf("I am parent after fork(), child is %d\n", pid);
+	pid = fork();
+	if (pid == 0) 
+		{
+		printf("I am child after fork(), launching free -h\n");
+		char *args[]= { "free", "-h" , NULL};
+		execvp("free", args);
+		}
+	else printf("I am parent after fork(), child is %d\n", pid);
+	return 0;
+}
